03895e0302f2a41db80fe063dfd0813255c44080
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-01-31 08:47:06 +0200

    Create teams.tf

---
+++ pagerduty/teams.tf
@@ -0,0 +1,12 @@
+resource "pagerduty_team" "job_scheduler" {
+  name        = "Job scheduler"
+}
+resource "pagerduty_team" "time_clock" {
+  name        = "Time clock"
+}
+resource "pagerduty_team" "cross_platform" {
+  name        = "Cross platform"
+}
+resource "pagerduty_team" "chat" {
+  name        = "Chat"
+}
