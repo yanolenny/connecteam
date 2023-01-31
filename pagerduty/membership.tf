a5dee711cc0f16d13c8af1789b2fdb3393755727
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-01-31 09:17:25 +0200

    Create membership.tf

---
+++ pagerduty/membership.tf
@@ -0,0 +1,16 @@
+resource "pagerduty_team_membership" "foo" {
+  user_id = pagerduty_user.foo.id
+  team_id = pagerduty_team.foo.id
+  role    = "responder"
+}
+
+  pagerduty_user.nadav_luzzato.id
+  pagerduty_user.ariel_nuriel.id
+  pagerduty_user.tal_yehoshafat.id
+  pagerduty_user.or_mocha.id
+  pagerduty_user.yaakov_sanders.id
+  pagerduty_user.mor_zamir.id
+  pagerduty_user.dean_leshem.id
+  pagerduty_user.maor_shabo.id
+  pagerduty_user.israel_ben_kalifa.id
+  pagerduty_user.golan_dor.id
