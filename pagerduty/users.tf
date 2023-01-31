a897ebd3b47cbd0f2a9e23c128a0f01a148db8fa
Yaniv Levi <121815406+yanolenny@users.noreply.github.com>
2023-01-31 09:05:46 +0200

    Create users.tf

---
+++ pagerduty/users.tf
@@ -0,0 +1,50 @@
+resource "pagerduty_user" "nadav_luzzato" {
+  name  = "Nadav Luzzato"  
+  email = "nadav@connecteam.com"
+}
+
+resource "pagerduty_user" "ariel_nuriel" {
+  name  = "Ariel Nuriel"  
+  email = "ariel@connecteam.com"
+}
+
+resource "pagerduty_user" "tal_yehoshafat" {
+  name  = "Tal Yehoshafat"  
+  email = "tal@connecteam.com"
+}
+
+resource "pagerduty_user" "or_mocha" {
+  name  = "Or Mocha" 
+  email = "or@connecteam.com"
+}
+
+resource "pagerduty_user" "yaakov_sanders" {
+  name  = "Yaakov Sanders"
+  email = "yaakov@connecteam.com"
+}
+
+resource "pagerduty_user" "mor_zamir" {
+  name  = "Mor Zamir"
+  email = "mor@connecteam.com"
+}
+
+resource "pagerduty_user" "dean_leshem" {
+  name  = "Dean Leshem"  
+  email = "dean@connecteam.com"
+}
+
+resource "pagerduty_user" "maor_shabo" {
+  name  = "Maor Shabo"
+  email = "maor@connecteam.com"
+}
+
+resource "pagerduty_user" "israel_ben_kalifa" {
+  name  = "Israel Ben-Kalifa"
+  email = "israelbk@connecteam.com"
+}
+
+resource "pagerduty_user" "golan_dor" {
+  name  = "Golan Dor"
+  email = "golan@connecteam.com"
+}
+
